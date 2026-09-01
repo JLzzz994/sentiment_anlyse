4ae47001768dc5a6b27548ba2848646862522458
JLzzz994 <56946379+JLzzz994@users.noreply.github.com>
2026-09-01 16:26:37 +0800

    feat: mount ecommerce demo page

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.dependencies import get_lifecycle_manager
 from app.exceptions.exception_handlers import register_exception_handlers
 from app.routers.rest.host_router import host_router
+from app.routers.rest.demo_router import demo_router
 from app.routers.rest.report_router import report_router
 from app.routers.rest.research_router import research_router
 from app.routers.rest.system_router import system_router
@@ -23,6 +24,7 @@ app = FastAPI(lifespan=lifespan)
 
 register_exception_handlers(app)
 
+app.include_router(demo_router)
 app.include_router(research_router)
 app.include_router(host_router)
 app.include_router(report_router)
